ae84157fa9ff30ad16ece9d50a4ea1cc6bc675cc
Oscar Z <ozamata@unsa.edu.pe>
2022-06-28 00:58:53 -0500

    formcontacto2

---
+++ app/firebaseConfig.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyDR6wUWtcKlbjR3nep4yI-oiwd5DiP0rLo",
+  "authDomain": "portafolio15.firebaseapp.com",
+  "projectId": "portafolio15",
+  "storageBucket": "portafolio15.appspot.com",
+  "messagingSenderId": "828187663344",
+  "appId": "1:828187663344:web:c8ac8ec50a77a6e6501b35",
+  "measurementId": "G-DFSCS99Y07",
+  "databaseURL":""
+}
